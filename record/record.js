1e5ba0afa22645798cb069ab4c561719e8637248
Carles Antón <carles.anton97@gmail.com>
2025-02-19 19:26:49 +0100

    feat: basic Recorder class structure using p5.capture

---
+++ record/record.js
@@ -0,0 +1,222 @@
+import { 
+    create_number_input_slider_and_number,
+    create_daisyui_expandable_card,
+    createToggleButton,
+    indentDiv,
+    createRecordStopButton,
+  } from '../ui.js'
+
+// Disable Default UI to create our own
+P5Capture.setDefaultOptions({
+    disableUi: true,
+    quality: 1,
+});
+
+export class Recorder {
+    static defaultFPS = 30;
+    static defaultFormat = 'webm'
+    static defaultRecordForSetDuration = false;
+    static defaultDuration = 2;
+    static defaultAutoSaveDuration = null;
+    static defaultUseSameFPS = true;
+    static defaultQuality = 0.1;
+
+    constructor (canvas) {
+        this.recorderInputs = null;
+        this.recorder = P5Capture.getInstance();
+        
+        // defaults
+        this.fps = Recorder.defaultFPS;
+        this.recordForSetDuration = Recorder.defaultRecordForSetDuration;
+        this.duration = Recorder.defaultDuration;
+        this.useSameFPS = Recorder.defaultUseSameFPS;
+        this.format = Recorder.defaultFormat;
+    }
+
+    setRecord(record){
+        console.log('record', record)
+        if (record) {
+            console.log('this.recordForSetDuration', this.recordForSetDuration)
+            let duration = this.recordForSetDuration ? this.duration : null;
+            duration *= this.recorder.framerate;
+            console.log('Recording:')
+            console.log('   FPS:', this.fps)
+            console.log('   format:', this.format)
+            console.log('   duration (frames):', duration)
+            console.log('   quality:', this.quality)
+            this.recorder.start(
+                {
+                    duration: duration,
+                    quality: this.quality,
+                }
+            )
+        }
+        else {
+            this.recorder.stop()
+        }
+
+    }
+
+    setUseSameFPS (value) {
+        this.useSameFPS = value;
+        const fpsSlider = this.recorderInputs['fpsDiv']
+
+        if (this.useSameFPS) {
+            console.log('Record: Using same FPS as main artwork');
+            fpsSlider.style.display = "none";
+        } else {
+            console.log('Record: Not sing same FPS as main artwork');
+            fpsSlider.style.display = "";
+        }
+    }
+    
+    setFPS(fps) {
+        this.fps = parseInt(fps);
+        this.recorder.framerate = this.fps;
+    }
+
+    setQuality(quality) {
+        this.quality = parseFloat(quality);
+        this.recorder.quality = this.quality;
+    }
+
+    setRecordForSetDuration (value) {
+        this.recordForSetDuration = value;
+        const durationSlider = this.recorderInputs['durationDiv']
+
+        if (!this.recordForSetDuration) {
+            console.log('Record: Recording Forever');
+            durationSlider.style.display = "none";
+        } else {
+            console.log('Record: Recording for set duration');
+            durationSlider.style.display = "";
+        }
+    }
+    
+    setDuration(duration) {
+        this.duration = duration;
+        this.recorder.duration = this.duration;
+    }
+
+    getProgress() {
+        let progress = this.recorder.recorder?.count;
+        if (!progress) {
+            progress = 0;
+        }
+        return progress;
+    }
+
+    framesToMSM(frames) {
+        // Calculate total milliseconds
+        const totalMilliseconds = (frames / this.fps) * 1000;
+        
+        // Extract minutes, seconds, and milliseconds
+        const minutes = Math.floor(totalMilliseconds / 60000);
+        const seconds = Math.floor((totalMilliseconds % 60000) / 1000);
+        let milliseconds = ((totalMilliseconds % 60000) / 1000) - seconds;
+        milliseconds = parseFloat(milliseconds.toFixed(2).toString().split('.')[1]);
+
+        // Format numbers with leading zeros
+        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(2, '0')}`;
+    }
+    
+
+    updateUITimmer() {
+        const uiTimmer = this.recorderInputs['timerText'];
+        const currentProgress = this.getProgress();
+        const currentTimer = this.framesToMSM(currentProgress);
+
+        // console.log('currentTimer', currentTimer)
+        uiTimmer.textContent = currentTimer;
+    }
+
+    createSettingsCard() {
+        const elements_dict = {};
+        
+        // Create Main Card
+        const card = create_daisyui_expandable_card('recorderSettings', 'Record Video');
+        const cardBody = card.getElementsByClassName('collapse-content')[0];
+
+        // Record/Stop Button
+        const recordButton = createRecordStopButton(
+            this.framesToMSM(0),
+            (a) => {
+                this.setRecord(a);
+            }
+        );
+        elements_dict['record'] = recordButton.getElementsByTagName('button')[0];
+        elements_dict['timerText'] = recordButton.getElementsByTagName('text')[0];
+
+        // FPS
+        const sameFPSButton = createToggleButton('Same FPS as Artwork', (a) => {
+            this.setUseSameFPS(a.target.checked);
+        }, this.useSameFPS);
+        elements_dict['sameFPSButton'] = sameFPSButton.getElementsByTagName('button')[0];
+
+        const fps = create_number_input_slider_and_number(
+            'FPS',
+            'FPS',
+            Recorder.defaultFPS,
+            1,
+            300,
+            (e) => {this.setFPS(e)},
+        );
+
+        indentDiv(fps, '30px');
+        fps.style.display =  "" ? this.useSameFPS: "none"; // Hide at first if must be hiden
+        elements_dict['fpsInput'] = fps.getElementsByTagName('input')[0];
+        elements_dict['fpsDiv'] = fps;
+
+        // Duration
+        const recordForSetDuration = createToggleButton('Record For Set Duration', (a) => {
+            this.setRecordForSetDuration(a.target.checked);
+        }, this.recordForSetDuration);
+        elements_dict['sameFPSButton'] = sameFPSButton.getElementsByTagName('button')[0];
+        
+        const duration = create_number_input_slider_and_number(
+            'Duration',
+            'Duration (s)',
+            this.duration,
+            1,
+            300,
+            (e) => {this.setDuration(e)},
+        );
+
+        indentDiv(duration, '30px');
+        duration.style.display =  "" ? this.recordForSetDuration: "none"; // Hide at first if must be hiden
+        elements_dict['durationInput'] = duration.getElementsByTagName('input')[0];
+        elements_dict['durationDiv'] = duration;
+
+        const quality = create_number_input_slider_and_number(
+            'recordQuality',
+            'Quality',
+            Recorder.defaultQuality,
+            0.,
+            1.,
+            (e) => {this.setQuality(e)},
+            0.05,
+        );
+        elements_dict['quality'] = quality.getElementsByTagName('input')[0];
+
+        cardBody.appendChild(recordButton);
+        // cardBody.appendChild(timerText);
+        cardBody.appendChild(document.createElement('br'));
+        cardBody.appendChild(recordForSetDuration);
+        cardBody.appendChild(duration);
+        cardBody.appendChild(document.createElement('br'));
+        cardBody.appendChild(sameFPSButton);
+        cardBody.appendChild(fps);
+        cardBody.appendChild(document.createElement('br'));
+        cardBody.appendChild(quality);
+
+        elements_dict['main-toolbar'] = card;
+        this.recorderInputs = elements_dict;
+
+        // Update Timmer every 10 milliseconds (0.1 second)
+        this.updateUITask = setInterval(() => {
+            this.updateUITimmer();
+        }, 10);
+
+        return elements_dict;
+    }
+}
